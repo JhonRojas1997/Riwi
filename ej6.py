a60872a96f550f0a79924e8ffee25c57bccf4571
JhonRojas1997 <113646996+JhonRojas1997@users.noreply.github.com>
2025-04-23 00:30:29 -0500

    Delete ej6.py

--- ej6.py
+++
@@ -1,8 +0,0 @@
-secreto = 10
-numero = int(input("digite un numero"))
-if numero == secreto:
-    print("el numero es correcto")
-if numero < secreto:
-    print("el numero es Menor buen intento")
-if numero > secreto:
-    print("el numero es Mayor buen intento")
